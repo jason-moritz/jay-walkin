febff1f8cff9054bf298aba0b10bd0ca66a9442d
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-10-08 11:36:56 -0400

    updated logic in models/products.js

--- models/product.js
+++ models/product.js
@@ -8,6 +8,7 @@ const Product = new Schema(
     description: { type: String, required: true },
     price: { type: String, required: true },
     category: { type: String, required: true },
+    brand: { type: String, required: true },
     gender: { type: String, required: true },
   },
   { timestamps: true }
