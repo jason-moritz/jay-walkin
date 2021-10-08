3c07498bdb67b46bc8228a79e2c31e46df6fe743
digallardox <digallardox@gmail.com>
2021-10-08 14:59:27 -0500

    site title and app.js setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
-function App() {
+export default const App = () => {
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+      const fetchUser = async () => {
+        const user = await verifyUser()
+        user ? setUser(user) : setUser(null)
+      }
+      fetchUser()
+    },[])
+
   return (
     <div className="App">
+      <Switch>
+        
+      </Switch>
 
     </div>
   );
 }
-
-export default App;
