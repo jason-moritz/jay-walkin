4673bdd2232270e6d8ab5d032f914153f0046c4e
Jason Moritz <moritz.jason@gmail.com>
2021-10-10 18:06:30 -0500

    Added base styling for Products.jsx

--- client/src/screens/Products/Products.jsx
+++ client/src/screens/Products/Products.jsx
@@ -2,6 +2,10 @@ import { useState, useEffect } from "react";
 import Layout from "../../components/Layout/Layout";
 import Product from "../../components/Product/Product";
 import { getProducts } from "../../services/products";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import "./Products.css";
 
 
 export default function Products(props) {
@@ -18,15 +22,21 @@ export default function Products(props) {
     return (
         <Layout user={props.user}>
             <div className="products">
-                {products.map((product) => (
-                    <Product
-                        _id={product._id}
-                        name={product.name}
-                        imgURL={product.imgURL}
-                        price={product.price}
-                        key={product._id}
-                    />
-                ))}    
+            <Box>
+                <Card variant="outlined" className="cards-container">
+                    {products.map((product) => (
+                        <CardContent className="card-content-container">
+                            <Product
+                                _id={product._id}
+                                name={product.name}
+                                imgURL={product.imgURL}
+                                price={product.price}
+                                key={product._id}
+                            />
+                        </CardContent>
+                    ))}
+                </Card>
+            </Box>    
             </div>
         </Layout>
     )
