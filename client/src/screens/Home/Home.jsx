f043a80fa5e27514032bf3b6bdaa070d707b9e79
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-10-11 15:59:01 -0400

    Logic for Carousel Working

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,22 +1,36 @@
 import Layout from "../../components/Layout/Layout";
 import ProductCards from "../../components/ProductCards/ProductCards";
 import "./Home.css";
+import Carousel from "react-material-ui-carousel";
+import Slider from "../../components/Slider/Slider";
+import Banner from "../../assets/Banner.jpeg";
+import Banner2 from "../../assets/Banner2.png";
 
 export default function Home(props) {
-    return (
-        <Layout user={props.user}>
-            <div className="home-container">
-                <div className="banner">
-                    <img
-                        className="img-banner"
-                        src="https://wallpapercave.com/wp/wp2927406.jpg"
-                        alt="shoe-banner"
-                    />
-                </div>
-                <div className="latest-container">
-                    <ProductCards />
-                </div>
-            </div>
-        </Layout>
-    )
+  let items = [
+    {
+      name: "Random Name #1",
+      description: "This is the description for Name #1",
+      image: Banner,
+    },
+    {
+      name: "Random Name #2",
+      description: "Hello World!",
+      image: Banner2,
+    },
+  ];
+  return (
+    <Layout user={props.user}>
+      <div className="home-container">
+        <Carousel>
+          {items.map((item, i) => (
+            <Slider key={i} item={item} />
+          ))}
+        </Carousel>
+        <div className="latest-container">
+          <ProductCards />
+        </div>
+      </div>
+    </Layout>
+  );
 }
