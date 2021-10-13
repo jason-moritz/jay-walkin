81d2a4ab089049a9efe3891aec91f6a08746bfb5
Jason Moritz <moritz.jason@gmail.com>
2021-10-12 19:01:19 -0500

    Re-styled SignUp.jsx and SignIn.jsx for responsive design

--- client/src/screens/SignUp/SignUp.jsx
+++ client/src/screens/SignUp/SignUp.jsx
@@ -4,13 +4,7 @@ import Layout from "../../components/Layout/Layout";
 import { useHistory } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import Typography from "@mui/material/Typography";
+import { Box, Card, CardContent, CardActions, Button, TextField, Typography, Container } from "@mui/material";
 import "./SignUp.css";
 
 
@@ -49,8 +43,10 @@ export default function SignUp(props) {
             setUser(user);
             toast(`Welcome ${form.username}!`);
             history.push("/");
+
         } catch (error) {
             console.error(error);
+
             if (error.response.data.error.includes("E11000" && "username")) {
                 setToggle2((prevToggle) => !prevToggle);
             };
@@ -72,6 +68,7 @@ export default function SignUp(props) {
         e.preventDefault();
 
         setToggle((prevToggle) => !prevToggle);
+
         setForm({
             username: "",
             email: "",
@@ -83,80 +80,117 @@ export default function SignUp(props) {
     return (
         <Layout user={props.user}>
             <div className='container-sign-up'>
-                <Typography sx={{ fontSize: 24 }} color="text.secondary" gutterBottom>
-                    Sign Up
-                </Typography>
-                <Box 
-                    className="box-sign-up"
-                    sx={{"& .MuiTextField-root": { m: 1, width: "45ch" }}}
-                    noValidate
-                    autoComplete="off"
+                <Container 
+                    maxWidth="sm" 
+                    minWidth="xs"
+                    sx={{ 
+                        display: "flex", 
+                        flexDirection: "column", 
+                        alignItems: "center", 
+                        textAlign: "center"
+                    }}
                 >
-                    <Card className="card-sign-up">
-                        <CardContent 
-                            className="card-content-sign-up">
-                            <form className="form-sign-up" onSubmit={form.password === form.passwordConfirmation ? onSignUp : handleToggle}>
-                                <TextField
-                                    label="Username"
-                                    value={username}
-                                    name="username"
-                                    required
-                                    autoFocus
-                                    onChange={handleChange}
-                                />
-                                <TextField
-                                    label="Email"
-                                    value={email}
-                                    name="email"
-                                    type="email"
-                                    required
-                                    onChange={handleChange}
-                                />
-                                <TextField
-                                    label="Password"
-                                    value={password}
-                                    inputProps={{ minLength: 8 }}
-                                    name="password"
-                                    type="password"
-                                    required
-                                    onChange={handleChange}
-                                />
-                                <TextField
-                                    label="Confirm Password"
-                                    value={passwordConfirmation}
-                                    inputProps={{ minLength: 8 }}
-                                    name="passwordConfirmation"
-                                    type="password"
-                                    required
-                                    onChange={handleChange}
-                                />
-                                {toggle === true ? 
-                                    <Typography sx={{ fontSize: 24 }} color="text.secondary" gutterBottom>
-                                        Error: Passwords Must Match 
-                                    </Typography>
-                                    : null
-                                }
-                                {toggle2 === true ? 
-                                    <Typography sx={{ fontSize: 24 }} color="text.secondary" gutterBottom>
-                                        Error: Username is already taken 
-                                    </Typography>
-                                    : null
-                                }
-                                {toggle3 === true ? 
-                                    <Typography sx={{ fontSize: 24 }} color="text.secondary" gutterBottom>
-                                        Error: Email is already in use 
-                                    </Typography>
-                                    : null
-                                }
-                                <CardActions>
-                                    <Button type="submit" className="submit-button">
-                                        Sign Up!
-                                    </Button>
-                                </CardActions>
-                            </form>
-                        </CardContent>
+                    <Card 
+                        sx={{ 
+                            width: "100%",
+                            display: "flex", 
+                            flexDirection: "column", 
+                            alignItems: "center" 
+                        }}
+                    >
+                        <Typography 
+                            sx={{ 
+                                fontSize: 24, 
+                                textAlign: "center" 
+                            }} 
+                            color="text.secondary" 
+                            gutterBottom
+                        >
+                            Sign In
+                        </Typography>
+                        <Box
+                            className="box-sign-up"
+                            sx={{ width: "100%",
+                                ".MuiTextField-root": { m: 1, width: ".75" }
+                            }}
+                            component="form"
+                            onSubmit={form.password === form.passwordConfirmation ? onSignUp : handleToggle}
+                        >
+                            <TextField
+                                label="Username"
+                                value={username}
+                                name="username"
+                                required
+                                autoFocus
+                                onChange={handleChange}
+                            />
+                            <TextField
+                                label="Email"
+                                value={email}
+                                name="email"
+                                type="email"
+                                required
+                                onChange={handleChange}
+                            />
+                            <TextField
+                                label="Password"
+                                value={password}
+                                inputProps={{ minLength: 8 }}
+                                name="password"
+                                type="password"
+                                required
+                                onChange={handleChange}
+                            />
+                            <TextField
+                                label="Confirm Password"
+                                value={passwordConfirmation}
+                                inputProps={{ minLength: 8 }}
+                                name="passwordConfirmation"
+                                type="password"
+                                required
+                                onChange={handleChange}
+                            />
+                            {toggle === true ? 
+                                <Typography 
+                                    sx={{ fontSize: 18 }} 
+                                    color="text-secondary" 
+                                    gutterBottom
+                                >
+                                    Error: Passwords Must Match 
+                                </Typography>
+                                : null
+                            }
+                            {toggle2 === true ? 
+                                <Typography 
+                                    sx={{ fontSize: 18 }} 
+                                    color="text-secondary" 
+                                    gutterBottom
+                                >
+                                    Error: Username is already taken 
+                                </Typography>
+                                : null
+                            }
+                            {toggle3 === true ? 
+                                <Typography 
+                                    sx={{ fontSize: 18 }} 
+                                    color="text-secondary" 
+                                    gutterBottom
+                                >
+                                    Error: Email is already in use 
+                                </Typography>
+                                : null
+                            }
+                            <Button type="submit">
+                                <Typography
+                                    gutterBottom
+                                    align="center"
+                                >
+                                    Sign In!
+                                </Typography>
+                            </Button>
+                        </Box>
                     </Card>
-                </Box>
+                </Container>
             </div>
         </Layout>
     )
