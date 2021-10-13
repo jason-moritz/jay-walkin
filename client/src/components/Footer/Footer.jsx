48880930db906c5f31b60865f4a1dea5bf3b18d0
Arsenalfutbol14 <mczanaty@gmail.com>
2021-10-13 13:56:08 -0400

    Updated footer with social media logos,hover effect on the cards, and increased latest hotness size

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,4 +1,7 @@
 import GitHubIcon from '@mui/icons-material/GitHub';
+import FaceBookIcon from '@mui/icons-material/Facebook'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import InstagramIcon from '@mui/icons-material/Instagram'
 import "./Footer.css";
 
 export default function Footer() {
@@ -10,6 +13,28 @@ export default function Footer() {
                 alt="link-github"
             >
                 <GitHubIcon className="icon-github"/>
+            </a>
+            <a
+                className="link-facebook"
+                href="https://www.facebook.com/"
+                alt="link-facebook"
+            >
+                <FaceBookIcon className="icon-facebook"/>
+        </a>
+      
+            <a
+                className="link-twitter"
+                href="https://twitter.com/?lang=en/"
+                alt="link-twitter"
+            >
+                <TwitterIcon className="icon-twitter"/>
+            </a>
+            <a
+                className="link-instagram"
+                href="https://www.instagram.com/"
+                alt="link-instagram"
+            >
+                <InstagramIcon className="icon-instagram"/>
             </a>
         </div>
     )
