4363c00b429a45d957414f165ab40d7b92f5d1df
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-10-13 12:35:27 -0400

    slider

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -4,30 +4,30 @@ import "./Home.css";
 import Carousel from "react-material-ui-carousel";
 import Slider from "../../components/Slider/Slider";
 import Banner1 from "../../assets/Banner1.jpeg";
-import Banner from "../../assets/Banner.jpeg";
-import Banner3 from "../../assets/Banner3.jpeg";
+import Banner2 from "../../assets/Banner2.jpg";
+import Banner6 from "../../assets/Banner6.jpg";
 
 export default function Home(props) {
   let items = [
     {
-      name: "FIND YOUR STYLE",
-      description: "JayWalkin offers a wide collection of shoes",
-      image: Banner1,
+      name: "EXPLORE",
+      description: "Adventure awaits with JayWalkin",
+      image: Banner6,
       link: "/products/",
       button: "Shop Now",
     },
     {
-      name: "ALWAYS ON YOUR GAME",
+      name: "VIP MEMBER",
       description:
         "Sign Up for JayWalkin to receive the latest updates on new shoe DROPS",
-      image: Banner,
+      image: Banner2,
       link: "/sign-up/",
       button: "Sign Up Today",
     },
     {
-      name: "REALIZE YOUR IDENTITY",
-      description: "Pick out a shoe that defines YOU",
-      image: Banner3,
+      name: "FIND YOUR STYLE",
+      description: "JayWalkin offers a wide collection of shoes",
+      image: Banner1,
       link: "/products/",
       button: "Shop Now",
     },
