41f1998da4ec9c18a9cb3e1e0ea18a2cec704d64
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-10-14 15:32:40 -0400

    added logic to Search folder and added logic to screens/products/products.js

--- client/src/screens/Products/Products.jsx
+++ client/src/screens/Products/Products.jsx
@@ -1,12 +1,18 @@
 import { useState, useEffect } from "react";
 import Layout from "../../components/Layout/Layout";
 import ProductCard from "../../components/ProductCard/ProductCard";
+import Search from "../../components/Search/Search";
+import Sort from "../../components/Sort/Sort";
+import { AZ, ZA, lowestFirst, highestFirst } from "../../utils/sort";
 import { getProducts } from "../../services/products";
 import { Box, Card, Typography } from "@mui/material/";
 import "./Products.css";
 
 export default function Products(props) {
   const [products, setProducts] = useState([]);
+  const [searchResult, setSearchResult] = useState([]);
+  const [applySort, setApplySort] = useState(false);
+  const [sortType, setSortType] = useState("name-ascending");
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -15,6 +21,44 @@ export default function Products(props) {
     };
     fetchProducts();
   }, []);
+
+  const handleSort = (type) => {
+    if (type !== "" && type !== undefined) {
+      setSortType(type);
+    }
+
+    switch (type) {
+      case "name-ascending":
+        setSearchResult(AZ(searchResult));
+        break;
+      case "name-descending":
+        setSearchResult(ZA(searchResult));
+        break;
+      case "price-ascending":
+        setSearchResult(lowestFirst(searchResult));
+        break;
+      case "price-descending":
+        setSearchResult(highestFirst(searchResult));
+        break;
+      default:
+        break;
+    }
+  };
+
+  if (applySort) {
+    handleSort(sortType);
+    setApplySort(false);
+  }
+
+  const handleSearch = (event) => {
+    const results = products.filter((product) =>
+      product.name.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    setSearchResult(results);
+    setApplySort(true);
+  };
+
+  const handleSubmit = (event) => event.preventDefault();
 
   const CARDS = products.map((product, i) => (
     <div key={i} className="products-card-container">
@@ -32,6 +76,8 @@ export default function Products(props) {
 
   return (
     <Layout user={props.user}>
+      <Search onSubmit={handleSubmit} handleSearch={handleSearch} />
+      <Sort onSubmit={handleSubmit} handleSort={handleSort} />
       <Box
         sx={{
           display: "flex",
