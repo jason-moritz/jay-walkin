c45b18d004f111a3f88c55391d827ad37d7a24b4
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-10-14 15:50:39 -0400

    sort

--- client/src/utils/sort.js
+++ client/src/utils/sort.js
@@ -8,9 +8,9 @@ const compareKey = (key) => (a, b) => {
   return 0;
 };
 
-export const AZ = (arr) => arr.sort(compareKey("name"));
-export const ZA = (arr) => arr.sort(compareKey("name")).reverse();
-export const lowestFirst = (arr) =>
-  arr.sort((a, b) => parseInt(a.price) - parseInt(b.price));
-export const highestFirst = (arr) =>
-  arr.sort((a, b) => parseInt(b.price) - parseInt(a.price));
+export const AZ = (products) => products.sort(compareKey("name"));
+export const ZA = (products) => products.sort(compareKey("name")).reverse();
+export const lowestFirst = (products) =>
+  products.sort((a, b) => parseInt(a.price) - parseInt(b.price));
+export const highestFirst = (products) =>
+  products.sort((a, b) => parseInt(b.price) - parseInt(a.price));
